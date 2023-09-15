f7e2a36b4affb98f3c3dee088256afb4d03c17ac
asan7 <andsan@live.se>
2023-09-15 12:55:40 +0200

    commit to avoid merge issue

--- include/raven_debug.h
+++ include/raven_debug.h
@@ -3,4 +3,13 @@
 
 void infobox(const char* format, ...);
 
+/**
+ * @brief Creates a breakpoint at the specified address. Use the Windows
+ *        debugging API to debug the breakpoint.
+ * 
+ * @param address       The address to insert the breakpoint at
+ * @param original_byte An integer pointer which will contain the original byte at the address
+*/
+void breakpoint(void* address, uint8_t* original_byte);
+
 #endif
--- src/debug/raven_debug.c
+++ src/debug/raven_debug.c
@@ -1,4 +1,5 @@
 #include "raven_debug.h"
+#include "raven_memory.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <windows.h>
@@ -10,4 +11,10 @@ void infobox(const char* format, ...){
     vsprintf_s(msg, 256, format, args);
     va_end(args);
     MessageBox(NULL, msg, "Info", MB_OK);
+}
+
+void breakpoint(void* address, uint8_t* original_byte){
+    const uint8_t breakpoint = 0xCC;
+    protected_write(original_byte, address, 1);
+    protected_write(address, &breakpoint, 1);
 }
